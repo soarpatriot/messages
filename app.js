ba469b60b2174e98d9b2fe5ddcc2573f94b88bf8
Soar <85624529@qq.com>
2016-03-16 18:23:40 +0800

    first messages server

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var port = 3000;
+var logger = require('tracer').console();
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+  socket.on('overtimes',function(data){
+    logger.info('overtimes: '+ data);
+    socket.emit('overtimes',data); 
+  })
+  socket.on('event', function(data){
+    logger.info('event data: '+ data);
+  });
+  socket.on('disconnect', function(){
+    logger.warn('disconnect socket io server');
+  });
+});
+server.listen(port);
+logger.info('server started at: '+ port);
